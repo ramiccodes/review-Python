bd1c6cd0a6c7a44bb9ecc5e7b971bc821aa82c78
Ramic <ramlag1819@gmail.com>
2023-07-25 22:01:48 -0700

    Exceptions lesson

---
+++ 6-exceptions.py
@@ -0,0 +1,9 @@
+# Exceptions
+numbers = [1, 2]
+# print(numbers[3])
+
+# An exception is an error that terminates the execution of the program
+
+age = int(input("Age: "))
+# The code above throws an exception when passed a string because it cannot
+# convert it into an integer
