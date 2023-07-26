11354d36dd48b1631214aca6541920ad22089ea1
Ramic <ramlag1819@gmail.com>
2023-07-26 09:41:00 -0700

    Classes lesson

---
+++ 7-classes.py
@@ -0,0 +1,10 @@
+# Classes
+# - A blueprint for creating new objects
+x = 1
+print(type(x))  # class int
+
+# Every object that we have in Python is created using a class
+# Object - Instance of a class
+
+# Class: Human
+# Object: John, Mary, Jack
