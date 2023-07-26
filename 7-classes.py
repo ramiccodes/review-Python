102d4568749e2ae63dfe327b4019b3c8ae509ed3
Ramic <ramlag1819@gmail.com>
2023-07-26 10:01:55 -0700

    Constructors lesson

--- 7-classes.py
+++ 7-classes.py
@@ -27,3 +27,23 @@ print(type(point))  # class __main__.Point
 # isinstance function finds out whether an object is an instance of a given class
 print(isinstance(point, Point))
 print(isinstance(point, int))
+
+# Constructors
+# - A special method that is called when we create a new Point object
+# __init__ is a special method called the magic method, this one in particular is called the constructor
+# after adding self, we can add extra parameters to the constructor
+# self is reference to the current Point object
+
+
+class PointArgs:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+
+point_coordinates = PointArgs(1, 2)
+print(point_coordinates.y)
+point_coordinates.draw()
