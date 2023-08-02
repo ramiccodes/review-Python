b43305ed52c888647b68a9d43177b2a067345125
Ramic <ramlag1819@gmail.com>
2023-08-02 15:26:56 -0700

    Python Standard Library lesson

---
+++ 9-py-standard-lib.py
@@ -0,0 +1,11 @@
+# Python Standard Library
+# Python has a batteries included philosophy which means it comes with a comprehensive
+# library of packages and modules that provide common feature that we need when building
+# real applications
+
+# We're going to learn how to work with:
+# - Files and directories
+# - SQLite databases
+# - Date/Time
+# - Generate Random Values
+# - Send emails
